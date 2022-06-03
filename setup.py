9f06e1877d746b3e55165cb407c42da28ec4333a
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-03 12:12:58 +0100

    Updated setup.py long description!

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 # Setting up
 setup(
     name='bc-count',
@@ -9,8 +12,9 @@ setup(
     author='nemo256 (Amine Neggazi)',
     author_email='<neggazimedlamine@gmail.com>',
     description='Count blood cells',
-    long_description='Count red, white blood cells to detect various diseases such as blood cancer (leukemia), lower red blood cells (anemia)...',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/nemo256/bc-count',
     install_requires=['opencv-python', 'tensorflow', 'numpy'],
     keywords=['python', 'artificial intelligence', 'deep learning', 'blood cells', 'image segmentation', 'unet'],
