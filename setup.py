cdf9bf16cf353ed74ba627ebbdcd3b07d487cff0
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-03 10:43:34 +0100

    Updated gitignore!

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Count blood cells',
     long_description='Count red, white blood cells to detect various diseases such as blood cancer (leukemia), lower red blood cells (anemia)...',
     packages=find_packages(),
-    url='https://github.com/nemo256/cell-count'
+    url='https://github.com/nemo256/cell-count',
     install_requires=['opencv-python', 'tensorflow', 'numpy'],
     keywords=['python', 'artificial intelligence', 'deep learning', 'blood cells', 'image segmentation', 'unet'],
     classifiers=[
@@ -20,7 +20,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
-    ]
+    ],
     extras_require={
         'testing': ['pytest'],
     }
