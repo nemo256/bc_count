28fcfc87fdf699e93b1cdff0caee656ea92da2bf
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-05 10:11:36 +0100

    Paperspace Test!

--- bc-count/config.py
+++ bc-count/config.py
@@ -1,5 +1,5 @@
 # global variables
-cell_type       = 'red'              # red, white or platelets
+cell_type       = 'white'              # red, white or platelets
 input_shape     = (188, 188, 3)
 output_shape    = (100, 100, 1)
 padding         = [200, 100]
