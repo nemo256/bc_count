408d9c14a1e8b10b8568bba974134567644d98b1
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-05 18:39:18 +0100

    Testing on paperspace (load each image seperately)!

--- bc-count/main.py
+++ bc-count/main.py
@@ -120,10 +120,11 @@ def train(model_name='mse', epochs=100):
             train_img_list,
             train_mask_list,
         )
-        test_dataset = generate_test_dataset(
-            test_img_list,
-            test_mask_list,
-        )
+        for i in len(test_img_list):
+            test_dataset += [generate_test_dataset(test_img_list[i],
+                                                   test_mask_list[i])]
+
+    print(len(test_dataset))
 
     # # initializing the do_unet model
     # model = do_unet()
