76ce2cd7cc36b1d18f1a3449964c3dbbf85144e8
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-22 20:33:06 +0100

    Default commit message!

--- bc-count/model.py
+++ bc-count/model.py
@@ -147,7 +147,7 @@ def do_unet():
         model.compile(loss='mse',
                       loss_weights=[0.1, 0.9],
                       optimizer=opt,
-                      metrics=['accuracy', 'iou'])
+                      metrics=['accuracy'])
     elif cell_type == 'wbc' or cell_type == 'plt':
         model.compile(loss='mse',
                       optimizer=opt,
