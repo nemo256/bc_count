68dc98045b232518e4471ceb33ef2296ad85dbff
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-23 12:57:34 +0100

    Default commit message!

--- bc-count/model.py
+++ bc-count/model.py
@@ -292,7 +292,7 @@ def segnet():
                       optimizer=opt,
                       metrics='accuracy')
     elif cell_type == 'plt':
-        model.compile(loss=focal_tversky,
+        model.compile(loss='mse',
                       optimizer=opt,
                       metrics=[mean_iou, dsc, tversky, 'accuracy'])
     return model
