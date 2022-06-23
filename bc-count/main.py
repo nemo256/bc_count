8acf080a7aa55257fd4946b90af7fc021a325ee9
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-23 16:51:14 +0100

    updated main!

--- bc-count/main.py
+++ bc-count/main.py
@@ -608,7 +608,7 @@ if __name__ == '__main__':
     The main function, which handles all the function call
     (later on, this will dynamically call functions according user input)
     '''
-    train('plt_segnet', epochs=12)
+    # train('plt_segnet', epochs=12)
     # evaluate(model_name='plt_segnet')
     # image = 'Im037_0'
     # predict(imgName=image)
@@ -627,5 +627,5 @@ if __name__ == '__main__':
     # count('threshold_denoise.png', image)
     # component_labeling('threshold_denoise.png', image)
 
-    # predict_all_idb()
+    predict_all_idb()
 
